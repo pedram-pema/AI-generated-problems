293d0fc5276b41fda12f8095096fe90a4123d1b6
pedram-pema <studentpedramp@gmail.com>
2024-01-04 07:19:39 +0330

    Solve the exercise

--- javascript/04_iteration/specialSum.js
+++ javascript/04_iteration/specialSum.js
@@ -1,35 +1,23 @@
 function calculateSpecialSum(arr) {
-    /* let evensSum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (i % 2 === 0) {
-            evensSum += arr[i];
-        }
-    }
-
+    let evensSum = 0;
     let odds = 0;
+  
     for (let i = 0; i < arr.length; i++) {
-        if (i % 2 === 1 && arr[i] >= arr[i - 1]) {
-            odds += arr[i] - evensSum;
-        } else if (i % 2 === 1 && arr[i] < arr[i - 1]) {
-            odds += arr[i] + evensSum;
-        }
+      if (i % 2 === 0) {
+        evensSum += arr[i];
+      }
     }
 
-    return evensSum + odds; */
-
-    let specialSum = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (i % 2 === 0) { // even index
-            specialSum += arr[i];
-        } else { // odd index
-            if (arr[i] > arr[i - 1]) {
-                specialSum -= arr[i];
-            } else {
-                specialSum += arr[i];
-            }
+      if (i % 2 === 1) {
+        if (arr[i] >= arr[i - 1]) {
+          odds += arr[i] - evensSum;
+        } else if (arr[i] < arr[i - 1]) {
+          odds += arr[i] + evensSum;
         }
+      }
     }
-    return specialSum;
+    
+    return evensSum + odds;
 }
-
 module.exports = calculateSpecialSum;
